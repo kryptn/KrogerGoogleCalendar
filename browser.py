3448f9c5c7de05263e1a914a6159308408b3c9f4
David <kryptn@gmail.com>
2015-05-10 13:45:40 -0700

    Separating logic

---
+++ browser.py
@@ -0,0 +1,92 @@
+import re
+from contextlib import contextmanager
+
+from bs4 import BeautifulSoup
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+
+@contextmanager
+def driver():
+
+    """ Context manager for both the selenium webdriver
+    and the pyvd display instance 
+
+    """
+
+    try:
+        display = Display(visible=0, size=(800,600))
+        display.start()
+        browser = webdriver.Firefox()
+        yield browser
+    finally:
+        b.quit()
+        display.stop()
+
+
+class KrogerBrowser(object):
+
+    main_url = 'http://greatpeople.me'
+    schedule_url = 'https://vpnb-cdc.kroger.com/EmpowerESS/,DanaInfo=wfm.kroger.com+Schedule.aspx'
+
+    def __init__(self, euid, password, **kwargs):
+        self.euid = euid
+        self.password = password
+
+        if 'main_url' in kwargs.keys():
+            self.main_url = kwargs['main_url']
+
+        if 'schedule_url' in kwargs.keys():
+            self.schedule_url = kwargs['schedule_url']
+
+    def login(self, browser):
+        """ Enters relevant info into the login page """
+        browser.find_element_by_name('KSWUSER').send_keys(self.user)
+        browser.find_element_by_name('PWD').send_keys(self.pwd)
+        browser.find_element_by_class_name('btn').click()
+
+    def fix_sessions(self, browser):
+        """ Attempts to fix a multiple-session error from the Juniper switch """
+        browser.find_element_by_name('postfixSID').click()
+        browser.find_element_by_name('btnContinue').click()
+
+    def parse_calendar(self, soup):
+        """
+        Parses the calendar found after logging in
+        regex is to get only the day items, which have class%d in their classes
+        """
+        schema = ('date','time','duration')
+        sched = {}
+        now = datetime.now()
+        for day in soup.find_all('li', class_=re.compile('[1-7]')):
+            d = list(day.stripped_strings)
+            if len(d) > 1:
+                d = dict(zip(schema, d))
+                r = {}
+                start, end = d['time'].split('-')
+                r['start'] = make_datetime(d['date'], start)
+                r['end'] = make_datetime(d['date'], end)
+                r['id'] = None
+                if r['start'] > now:
+                    sched[d['date']] = r
+
+        return sched
+
+    def navigate(self, browser, url):
+        browser.get(url)
+        #possible debug statement here
+
+    def get_schedule_source(self):
+        with driver() as browser:
+            self.navigate(browser, self.main_url)
+            login(browser)
+            if 'Confirm' in browser.title:
+                self.fix_sessions(browser)
+            self.navigate(browser, self.schedule_url)
+            soup = BeautifulSoup(browser.page_source)
+        return soup
+
+    def pull(self):
+        soup = self.get_schedule_source()
+        schedule = self.parse_calendar(soup)
+        return schedule
--- schedule.py
+++ schedule.py
@@ -1,4 +1,3 @@
-import re
 import json
 import pickle
 from httplib2 import Http
@@ -7,11 +6,9 @@ from datetime import datetime
 from contextlib import contextmanager
 
 from oauth2client.client import SignedJwtAssertionCredentials
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
-from pyvirtualdisplay import Display
 from apiclient.discovery import build
-from bs4 import BeautifulSoup
+
+from browser import KrogerBrowser
 
 with open('settings.json') as f:
     SETTINGS = json.loads(f.read())
@@ -27,51 +24,29 @@ def lazydb(filename):
     with open(filename,'w') as f:
         pickle.dump(db, f)
 
-@contextmanager
-def driver():
-    """ Creates a selenium webdriver object to manipulate """
-    try:
-        b = webdriver.Firefox()
-        b.get(SETTINGS['MAIN_URL'])
-        yield b
-    finally:
-        b.quit()
-
-def display(f):
-    """ Wrapper for pyvd display object """
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        try:
-            d = Display(visible=0, size=(800,600))
-            d.start()
-            r = f(*args, **kwargs)
-        finally:
-            d.stop()    
-        return r
-    return wrapper
-
-def make_datetime(dates, times):
+def make_datetime(date, time):
     dt = {'year':2015,
-          'month':int(dates.split('/')[0]),
-          'day':int(dates.split('/')[1])}
-    ampm = times[-1]
-    times = [int(x) for x in times[:-1].split(':')]
-    if ampm == 'p' and times[0] is not 12:
-        times[0] = times[0]+12
-    elif ampm == 'a' and times[0] is 12:
-        times[0] = 0
-    dt['hour'] = times[0]
-    dt['minute'] = times[1]
+          'month':int(date.split('/')[0]),
+          'day':int(date.split('/')[1])}
+    ampm = time[-1]
+    time = [int(x) for x in time[:-1].split(':')]
+    if ampm == 'p' and time[0] is not 12:
+        time[0] = time[0]+12
+    elif ampm == 'a' and time[0] is 12:
+        time[0] = 0
+    dt['hour'] = time[0]
+    dt['minute'] = time[1]
 
     return datetime(**dt)
 
 def build_event(start, end):
     """ Builds calendar object to pass to the api """
     event = {'summary': 'Work',
+             'location': SETTINGS['LOCATION']
              'start': {'dateTime': start.isoformat('T'),
-                       'timeZone': 'America/Los_Angeles'},
+                       'timeZone': SETTINGS['TIMEZONE']},
              'end': {'dateTime': end.isoformat('T'),
-                     'timeZone': 'America/Los_Angeles'},
+                     'timeZone': SETTINGS['TIMEZONE']},
              'attendees': [{'email': SETTINGS['EMAIL'],
                             'responseStatus': 'accepted'},],
              }
@@ -90,65 +65,6 @@ def api_service(endpoint, version):
     service = build(endpoint, version, http=creds.authorize(Http()))
     return service
 
-def enter_info(browser, user, pwd):
-    """ Enters relevant info into the login page """
-    browser.find_element_by_name('KSWUSER').send_keys(user)
-    browser.find_element_by_name('PWD').send_keys(pwd)
-    browser.find_element_by_class_name('btn').click()
-
-def fix_sessions(browser):
-    """ Attempts to fix a multiple-session error from the Juniper switch """
-    browser.find_element_by_name('postfixSID').click()
-    browser.find_element_by_name('btnContinue').click()
-
-
-def parse_calendar(soup):
-    """
-    Parses the calendar found after logging in
-    regex is to get only the day items, which have class%d in their classes
-    """
-    schema = ('date','time','duration')
-    sched = {}
-    now = datetime.now()
-    for day in soup.find_all('li', class_=re.compile('[1-7]')):
-        d = list(day.stripped_strings)
-        if len(d) > 1:
-            d = dict(zip(schema, d))
-            r = {}
-            start, end = d['time'].split('-')
-            r['start'] = make_datetime(d['date'], start)
-            r['end'] = make_datetime(d['date'], end)
-            r['id'] = None
-            if r['start'] > now:
-                sched[d['date']] = r
-
-    return sched
-
-@display
-def get_source():
-    """ Gets page source for schedule """
-    with driver() as browser:
-        if DEBUG: print browser.title
-        enter_info(browser,
-                   SETTINGS['EUID'],
-                   SETTINGS['PASSWORD'])
-        if DEBUG: print browser.title
-        if 'Confirm' in browser.title:
-            fix_sessions(browser)
-        if DEBUG: print browser.title
-        browser.get(SETTINGS['SCHEDULE_URL'])
-        if DEBUG: print browser.title
-        if 'Confirm' in browser.title:
-            fix_sessions(browser)
-            if DEBUG: print browser.title
-        soup = BeautifulSoup(browser.page_source)
-    return soup
-
-def get_schedule():
-    soup = get_source()
-    schedule = parse_calendar(soup)
-    return schedule
-
 def add_event(day):
     calendar = api_service('calendar','v3')
     event = build_event(day['start'],day['end'])
@@ -158,7 +74,8 @@ def add_event(day):
 
 def update():
     now = datetime.now()
-    schedule = get_schedule()
+    browser = KrogerBrowser(SETTINGS['EUID'], SETTINGS['PWD'])
+    schedule = browser.pull_schedule()
     
     with lazydb('lazydb.pk') as db:
 
